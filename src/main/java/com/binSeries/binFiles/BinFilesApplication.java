3c545de2f443af47f2c785d3629a7c9bfc2bc0bd
ga-bin <rlarkqlskb@naver.com>
2024-12-28 18:39:02 +0900

    [김가빈] add : binFiles 백엔드 최초 커밋

---
+++ src/main/java/com/binSeries/binFiles/BinFilesApplication.java
@@ -0,0 +1,13 @@
+package com.binSeries.binFiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binSeries/binFiles/BinFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binSeries.binFiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
